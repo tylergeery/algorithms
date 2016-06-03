3f5215c4e1cc673678d8faec4f68ac9b89badf16
tylergeery <tyler.geery@yahoo.com>
2016-06-03 11:23:33 -0700

    number of ways to climb staircase

---
+++ toolbox/staircase.py
@@ -0,0 +1,29 @@
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+'''
+A person can walk up a staircase by 1 or 2 steps at a time
+Find the total number of ways the person can climb a staircase with a given number of steps
+'''
+number_of_steps = 50
+
+'''
+n - number of steps
+'''
+def climb(n):
+    lookup = {
+        0: 0,
+        1: 1,
+        2: 2
+    }
+
+    if n <= 2:
+        return lookup[n]
+
+    for i in range(3,n):
+        lookup[i] = lookup[i-1] + lookup[i-2]
+
+    pp.pprint(lookup)
+    print "Total possible ways: " + str(lookup[n-1] + lookup[n-2])
+
+
+climb(number_of_steps)
